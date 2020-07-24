83053c49532adf2c72d04d2e394aa8d6dbf1335f
DmitriyLitvin <dimalitvin192.0@gmail.com>
2020-07-24 21:40:56 +0300

    selenium basic was done

---
+++ basic.py
@@ -0,0 +1,12 @@
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+
+from selenium import webdriver
+driver = webdriver.Chrome(executable_path="c:/selenium/chromedriver.exe")
+driver.get("https://www.seleniumeasy.com/test/bootstrap-alert-messages-demo.html")
+driver.find_element_by_id("autoclosable-btn-success").click();
+alertMessage = driver.find_element_by_class_name("alert, alert-success, alert-autocloseable-success").text
+assert alertMessage, "I'm an autocloseable success message. I will hide in 5 seconds."
